39e62817999d728fd0fc3ec584f28c4f57e59b71
danalvin <mungaiwaituika@gmail.com>
2018-09-14 17:32:56 +0300

    user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from .forms import RegistrationForm, LoginForm 
+from flask_login import LoginManager
 
 
 
@@ -8,5 +9,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '1q2w3e4r'
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
 db = SQLAlchemy(app)
+loginmanager = LoginManager(app) 
+loginmanager.login_view = 'login'
+loginmanager.login_message_category = 'info'
 
 from app import views
