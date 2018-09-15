345f6e07129709e9c042d0a48adc95ce1276b32d
danalvin <mungaiwaituika@gmail.com>
2018-09-15 17:06:41 +0300

    user authentication improved

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,13 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from .forms import RegistrationForm, LoginForm 
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '1q2w3e4r'
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-loginmanager = LoginManager(app) 
-loginmanager.login_view = 'login'
-loginmanager.login_message_category = 'info'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from app import views
